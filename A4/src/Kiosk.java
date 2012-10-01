c11412e64cf10d295ceced33d2bb6a35c5e8ca7f
mattloidolt <mattl@opdemand.com>
2012-10-01 15:51:02 -0600

    starting actual work on the kiosk

--- A4/src/Kiosk.java
+++ A4/src/Kiosk.java
@@ -3,21 +3,34 @@
  * thread. May use its own worker threads to make the GUI more interactive.
  */
 
+import java.awt.*;
 import javax.swing.*;
 
 
 public class Kiosk {
 	
-	public int kioskID ;
+	public static int kioskID = 0;
 
 	private static void createAndShowGUI() {
+		// variables
+		Color background = new Color(240, 125, 110) ;
+		Dimension size = new Dimension(800, 600) ;
+		// TODO: determine way to get these menu names automatically from the restaurant
+		String[] menuNames = { "Menu1", "Menu2", "Menu3" };
+		
+		
         //Create and set up the window.
-        JFrame frame = new JFrame("PIZZA_STORE_NAME");
+        JFrame frame = new JFrame("PIZZA_STORE_NAME Kiosk: " + kioskID);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
+        //TODO: add content here
+        JLabel label = new JLabel("Select Menu: ");
+        frame.getContentPane().add(label, BorderLayout.NORTH);
+        JComboBox menusList = new JComboBox(menuNames);
+        menusList.setSelectedIndex(0);
+        frame.setAlwaysOnTop(true) ;
+        frame.setMinimumSize(size) ;
+        frame.setBackground(background) ;
  
         //Display the window.
         frame.pack();
