8e1be50f7ef9ab3c4af54dd1732ae4861157c74b
mattloidolt <mattl@opdemand.com>
2012-10-02 12:28:54 -0600

    more work on kiosk, can't get internal frame to appear on top of main frame

--- A4/src/Kiosk.java
+++ A4/src/Kiosk.java
@@ -4,54 +4,74 @@
  */
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 
 public class Kiosk {
 	
 	public static int kioskID = 0;
+	private static GridBagConstraints gBC = new GridBagConstraints();
+	private static JFrame frame = new JFrame("PIZZA_STORE_NAME Kiosk: " + kioskID);
+
 
 	private static void createAndShowGUI() {
 		// variables
 		Color background = new Color(240, 125, 110) ;
-		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-		
-		
-		
+		final Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
 		
         // Create and set up the window.
-        JFrame frame = new JFrame("PIZZA_STORE_NAME Kiosk: " + kioskID);
         //TODO: Remove this and find way so that user cannot close window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(new GridBagLayout()); // this is the frame
-        GridBagConstraints gBC = new GridBagConstraints();
+        frame.getContentPane().setLayout(new GridBagLayout()); // this is the framing structure
         gBC.fill = GridBagConstraints.HORIZONTAL;
         
- 
         //###############  CONTENT  ######################
         // top line
         JLabel label = new JLabel("Select Menu: ");
-        gBC.anchor = GridBagConstraints.PAGE_START ;
-        gBC.weightx = 0.25 ;
+        gBC.weightx = 0.2 ;
         gBC.gridx = 0 ;
         gBC.gridy = 0 ;
         frame.getContentPane().add(label, gBC);
         
         // drop down to select menu
-        String[] menuNames = {"Menu 1", "Menu 2", "Menu 3"} ;
+        String[] menuNames = {"Select a Menu", "Menu 1", "Menu 2", "Menu 3"} ;
         JComboBox menusList = new JComboBox(menuNames);
         menusList.setSelectedIndex(0);
-        gBC.weightx = 0.75 ;
+        gBC.weightx = 0.8 ;
         gBC.gridx = 1;
         gBC.gridy = 0;
+        menusList.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e)
+            {
+        		JComboBox cb = (JComboBox)e.getSource();
+                String menuName = (String)cb.getSelectedItem();
+                if(!menuName.equals("Select a Menu")){
+                	JInternalFrame internalFrame = new JInternalFrame(menuName, false, true, false, false);
+                	internalFrame.setBounds(0, 50, size.width, size.height);
+                	internalFrame.setVisible(true);
+                	internalFrame.show();
+                	gBC.ipady = size.height-100;
+                	gBC.weightx = 0 ;
+                	gBC.gridwidth = 2;
+                	gBC.gridx = 0 ;
+                	gBC.gridy = 1 ;
+                	frame.getContentPane().add(internalFrame, gBC) ;
+                }
+            }}) ;
         frame.getContentPane().add(menusList, gBC) ;
         
-        // setting up internal frame to display actual menu
+        
+        // fill empty space
+        gBC.ipady = size.height-100 ;
+        gBC.weightx = 0 ;
+        gBC.gridwidth = 3 ;
+        gBC.gridx = 0 ;
+        gBC.gridy = 1 ;
+        frame.getContentPane().add(Box.createGlue(), gBC) ;
         
         
         
-        
-        frame.setAlwaysOnTop(true) ;
         frame.setMinimumSize(size) ;
         frame.setBackground(background) ;
  
