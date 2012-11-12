2b77ad4115563cb1c06e47f47d4a9cc3cbedbc6e
Zach McGaughey <zmcgaughey@gmail.com>
2012-11-11 22:25:41 -0700

    Added php file to get order

---
+++ A4/getOrder.php
@@ -0,0 +1,26 @@
+<?php
+     /**********************************************************************
+     Start of PHP
+     **********************************************************************/
+    
+    // establish connection
+    $con = mysql_connect("faure.cs.colostate.edu","loidolt","827375410");
+    if (!$con) {
+        die('Could not connect: ' . mysql_error());
+    }
+    
+    mysql_select_db("loidolt", $con);
+    
+    $safe_menu = mysql_real_escape_string($_POST["menu"]) ;
+    
+    $q=mysql_query("SELECT id, name, items FROM orders ;");
+    while($e=mysql_fetch_assoc($q))
+    $output[]=$e;
+    
+    print(json_encode($output));
+    
+    // close connection to database
+    mysql_close($con) ;
+
+
+?>
