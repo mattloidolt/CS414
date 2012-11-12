99a632e8b5db11c3ca1a1a679b7eba946a1d85fe
Zach McGaughey <zmcgaughey@gmail.com>
2012-11-11 22:49:27 -0700

    Updated php get order file

--- A4/src/controller/KitchenDisplayCont.java
+++ A4/src/controller/KitchenDisplayCont.java
@@ -6,6 +6,8 @@ package controller;
 
 import java.awt.Color;
 import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.* ;
 import java.util.*;
 import java.util.logging.Level;
@@ -21,30 +23,58 @@ public class KitchenDisplayCont {
     
     public static ArrayList<JLabel> getOrdersToDisplay(){
         ArrayList<JLabel> orders = new ArrayList<JLabel>() ;
-        try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
-                                           "user=pizzaStore&password=password");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT id, name, items FROM orders ;");
-            while(rs.next()) {
-                String order = "<html><h2>" + rs.getString(2) + "</h2> <table>" ;
-                String[] items = rs.getString(3).split("&%&") ;
-                order += "<!--&&&&" + rs.getString(1) + "&&&&-->";
-                for (int i=0 ; i < items.length ; i++) {
-                    order += "<tr> " + items[i] + "</tr>" ;
-                }
-                order += "</table> <br> </html>" ;
-                JLabel o = new JLabel(order) ;
-                o.setForeground(Color.white) ;
-                orders.add(o) ;
-            }
-            stmt.close() ;
-            rs.close() ;
-            conn.close() ;
-            
-        } catch (Exception ex) {
-            System.err.println(ex) ;
-        }
+        
+		String result = "";
+
+
+		try{
+			URL url = new URL("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getOrder.php");
+			URLConnection connection = url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			String tmp;
+			while ((tmp = in.readLine()) != null){
+				result = tmp.substring(1, tmp.length()-2);
+			}
+			in.close();
+			
+			
+			String[] list = result.split(",");			
+			for(String item: list){
+				item = item.substring(9, item.length()-2);
+//				orders.add(item);
+			}
+
+		}catch(Exception e){
+			System.err.println("Error in getting menu names "+e.toString());
+		}
+
+        
+        
+//        try {
+//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
+//                                           "user=pizzaStore&password=password");
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery("SELECT id, name, items FROM orders ;");
+//            while(rs.next()) {
+//                String order = "<html><h2>" + rs.getString(2) + "</h2> <table>" ;
+//                String[] items = rs.getString(3).split("&%&") ;
+//                order += "<!--&&&&" + rs.getString(1) + "&&&&-->";
+//                for (int i=0 ; i < items.length ; i++) {
+//                    order += "<tr> " + items[i] + "</tr>" ;
+//                }
+//                order += "</table> <br> </html>" ;
+//                JLabel o = new JLabel(order) ;
+//                o.setForeground(Color.white) ;
+//                orders.add(o) ;
+//            }
+//            stmt.close() ;
+//            rs.close() ;
+//            conn.close() ;
+//            
+//        } catch (Exception ex) {
+//            System.err.println(ex) ;
+//        }
         return orders ;
 
     }
