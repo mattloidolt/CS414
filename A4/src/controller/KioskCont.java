2848d68cab161229e4f1afc564eb17b023b0ce46
mattloidolt <mattl@opdemand.com>
2012-11-11 22:30:12 -0700

    saving order

--- A4/src/controller/KioskCont.java
+++ A4/src/controller/KioskCont.java
@@ -14,6 +14,14 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.sql.*;
 import java.util.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 
 /**
@@ -104,37 +112,49 @@ public class KioskCont {
 	 * all items are in one field in the database called 'items' delimited by '&%&'
 	 */
 	public static boolean saveOrder(ArrayList<String> orderItems) {
-		boolean success = true ;
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
-					"user=pizzaStore&password=password");
-			Statement stmt = conn.createStatement();
-			String query = "INSERT INTO orders "
-					+ "(name, phone, address, nameOnCard, creditCardNumber, expirationDate, items) VALUES('" ;
-			query += orderItems.get(0) + "', '" ; // name
-			query += orderItems.get(1) + "', '" ; // phone number
-			query += orderItems.get(2) + "', '" ; // address
-			query += orderItems.get(3) + "', '" ; // nameOnCard
-			query += orderItems.get(4) + "', '" ; // creditCardNum
-			query += orderItems.get(5) + "', '" ; // expirationDate
+		boolean success = true ;		
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("name", orderItems.get(0)));
+        nameValuePairs.add(new BasicNameValuePair("phone", orderItems.get(0)));
+        nameValuePairs.add(new BasicNameValuePair("address", orderItems.get(0)));
+        nameValuePairs.add(new BasicNameValuePair("nameOnCard", orderItems.get(0)));
+        nameValuePairs.add(new BasicNameValuePair("CCnum", orderItems.get(0)));
+        nameValuePairs.add(new BasicNameValuePair("expDate", orderItems.get(0)));
+        String items = "" ;
+        for (int i=6; i < orderItems.size(); i++) {
+                items += orderItems.get(i) + "&%&" ;
+        }
+        nameValuePairs.add(new BasicNameValuePair("order", items));
 
-			String items = "" ;
-			for (int i=6; i < orderItems.size(); i++) {
-				items += orderItems.get(i) + "&%&" ;
-			}
-			query += items + "') ;" ;
-			stmt.executeUpdate(query) ;
-			stmt.close() ;
-			conn.close() ;
 
-		}
-		catch(Exception e) {
-			System.err.print(e);
-			success = false ;
-		}
-		if (success) {
-			System.out.println("Order saved") ;
-		}
-		return success ;
+        try{
+            // http post
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/saveOrder.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+
+            //convert response to string
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+            }
+            is.close();
+
+            String result=sb.toString();
+            
+            System.out.println(result) ;
+
+        }catch(Exception e){
+                System.err.println("Error in saving order "+e.toString());
+        }
+        if (success) {
+                System.out.println("Order saved") ;
+        }
+        return success ;
 	}
 }
