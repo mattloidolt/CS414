4d4a37080224ef965248d121ef432376cac95954
Zach McGaughey <zmcgaughey@gmail.com>
2012-11-11 23:25:34 -0700

    Kitchen display pulls order from server

--- A4/src/controller/KitchenDisplayCont.java
+++ A4/src/controller/KitchenDisplayCont.java
@@ -20,10 +20,10 @@ import org.apache.tools.ant.DirectoryScanner;
  * @author mattloidolt
  */
 public class KitchenDisplayCont {
-    
-    public static ArrayList<JLabel> getOrdersToDisplay(){
-        ArrayList<JLabel> orders = new ArrayList<JLabel>() ;
-        
+
+	public static ArrayList<JLabel> getOrdersToDisplay(){
+		ArrayList<JLabel> orders = new ArrayList<JLabel>() ;
+		String order = "";
 		String result = "";
 
 
@@ -31,66 +31,71 @@ public class KitchenDisplayCont {
 			URL url = new URL("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getOrder.php");
 			URLConnection connection = url.openConnection();
 			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			
+
 			String tmp;
 			while ((tmp = in.readLine()) != null){
-				result = tmp.substring(1, tmp.length()-2);
+				result = tmp.substring(tmp.indexOf("items")+8, tmp.length()-6);
 			}
 			in.close();
-			
-			
-			String[] list = result.split(",");			
+
+
+			order = "<html> <table>" ;
+			String[] list = result.split("&%&");			
 			for(String item: list){
-				item = item.substring(9, item.length()-2);
-//				orders.add(item);
+				order += "<tr> " + item + "</tr>" ;
 			}
+			order += "</table> <br> </html>" ;
+
+			JLabel o = new JLabel(order) ;
+			o.setForeground(Color.white) ;
+			orders.add(o) ;
 
 		}catch(Exception e){
 			System.err.println("Error in getting menu names "+e.toString());
 		}
 
-        
-        
-//        try {
-//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
-//                                           "user=pizzaStore&password=password");
-//            Statement stmt = conn.createStatement();
-//            ResultSet rs = stmt.executeQuery("SELECT id, name, items FROM orders ;");
-//            while(rs.next()) {
-//                String order = "<html><h2>" + rs.getString(2) + "</h2> <table>" ;
-//                String[] items = rs.getString(3).split("&%&") ;
-//                order += "<!--&&&&" + rs.getString(1) + "&&&&-->";
-//                for (int i=0 ; i < items.length ; i++) {
-//                    order += "<tr> " + items[i] + "</tr>" ;
-//                }
-//                order += "</table> <br> </html>" ;
-//                JLabel o = new JLabel(order) ;
-//                o.setForeground(Color.white) ;
-//                orders.add(o) ;
-//            }
-//            stmt.close() ;
-//            rs.close() ;
-//            conn.close() ;
-//            
-//        } catch (Exception ex) {
-//            System.err.println(ex) ;
-//        }
-        return orders ;
 
-    }
-    
-    public static boolean archiveOrder(String orderID) {
-        try{
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
-                                           "user=pizzaStore&password=password");
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate("DELETE FROM orders WHERE id='" + orderID + "' ;");
-        }
-        catch(Exception e){
-            System.err.println(e);
-        }
-        return true ;        
-    }
+
+		//        try {
+		//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
+		//                                           "user=pizzaStore&password=password");
+		//            Statement stmt = conn.createStatement();
+		//            ResultSet rs = stmt.executeQuery("SELECT id, name, items FROM orders ;");
+		//            while(rs.next()) {
+		//                String order = "<html><h2>" + rs.getString(2) + "</h2> <table>" ;
+		//                String[] items = rs.getString(3).split("&%&") ;
+		//                order += "<!--&&&&" + rs.getString(1) + "&&&&-->";
+		//                for (int i=0 ; i < items.length ; i++) {
+		//                    order += "<tr> " + items[i] + "</tr>" ;
+		//                }
+		//                order += "</table> <br> </html>" ;
+		//                JLabel o = new JLabel(order) ;
+		//                o.setForeground(Color.white) ;
+		//                orders.add(o) ;
+		//            }
+		//            stmt.close() ;
+		//            rs.close() ;
+		//            conn.close() ;
+		//            
+		//        } catch (Exception ex) {
+		//            System.err.println(ex) ;
+		//        }
+		return orders ;
+
+	}
+
+	public static boolean archiveOrder(String orderID) {
+		try{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
+					"user=pizzaStore&password=password");
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate("DELETE FROM orders WHERE id='" + orderID + "' ;");
+		}
+		catch(Exception e){
+			System.err.println(e);
+		}
+		return true ;        
+	}
 }
 
 
