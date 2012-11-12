f212aa4ce0d21f8faa833e1248096795800065b6
Zach McGaughey <zmcgaughey@gmail.com>
2012-11-11 22:19:04 -0700

    Kiosk loads from server.

--- A4/src/controller/KioskCont.java
+++ A4/src/controller/KioskCont.java
@@ -5,141 +5,136 @@
 package controller;
 
 import java.io.BufferedReader;
+
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.*;
 import java.util.*;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 /**
  *
- * @author mattloidolt
+ * @author mattloidolt, Zach McGaughey
  */
 public class KioskCont {
-    public static ArrayList<String> getMenuNames(){
-        ArrayList<String> names = new ArrayList<String>() ;
-        String result = "";
+	@SuppressWarnings("null")
+	public static ArrayList<String> getMenuNames(){
 
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair("year","1980"));
+		ArrayList<String> names = new ArrayList<String>() ;
+		String result = "";
 
-        try{
-                // http post
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getMenus.php");
-                HttpResponse response = httpclient.execute(httppost);
-                HttpEntity entity = response.getEntity();
-                InputStream is = entity.getContent();
 
-                //convert response to string
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-        }
-        is.close();
+		try{
+			URL url = new URL("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getMenus.php");
+			URLConnection connection = url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			String tmp;
+			while ((tmp = in.readLine()) != null){
+				result = tmp.substring(1, tmp.length()-2);
+			}
+			in.close();
+			
+			
+			String[] list = result.split(",");			
+			for(String item: list){
+				item = item.substring(9, item.length()-2);
+				names.add(item);
+			}
 
-        result=sb.toString();
+		}catch(Exception e){
+			System.err.println("Error in getting menu names "+e.toString());
+		}
 
-        // parse JSON data and add to list
-        JSONArray jArray = new JSONArray(result);
-        for(int i=0;i<jArray.length();i++){
-                JSONObject json_data = jArray.getJSONObject(i);
-                names.add(json_data.getString("name")) ;
-        }
-        }catch(Exception e){
-                System.err.println("Error in getting menu names "+e.toString());
-        }
+		return names ;
 
-        return names ;
-    }
-    
-    public static ArrayList<String> getMenu(String menuName){
-        ArrayList<String> loadMenu = new ArrayList<String>();
-		
-        String result = "";
-
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair("menu", menuName));
-		
-	try{
-            // http post
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getMenu.php");
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
-            InputStream is = entity.getContent();
-
-            //convert response to string
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                    sb.append(line + "\n");
-            }
-            is.close();
-
-            result=sb.toString();
-
-            // parse JSON data and add to list
-            JSONArray jArray = new JSONArray(result);
-            loadMenu.add(jArray.getJSONObject(0).getString("menu")) ;
-            String elements[] = jArray.getJSONObject(1).getString("items").split("&&&");
-            loadMenu.addAll(Arrays.asList(elements));
-	}catch(Exception e){
-            System.err.println("Error in getting menu items "+e.toString());
 	}
+
+	public static ArrayList<String> getMenu(String menuName){
+		ArrayList<String> loadMenu = new ArrayList<String>();
+		String result = "";
 		
-	return loadMenu ;
-    }
-    
-    /*
-     * saves the order to the orders database
-     * all items are in one field in the database called 'items' delimited by '&%&'
-     */
-    public static boolean saveOrder(ArrayList<String> orderItems) {
-        
-        boolean success = true ;		
-        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-        nameValuePairs.add(new BasicNameValuePair("name", orderItems.get(0)));
-        nameValuePairs.add(new BasicNameValuePair("phone", orderItems.get(0)));
-        nameValuePairs.add(new BasicNameValuePair("address", orderItems.get(0)));
-        nameValuePairs.add(new BasicNameValuePair("nameOnCard", orderItems.get(0)));
-        nameValuePairs.add(new BasicNameValuePair("CCnum", orderItems.get(0)));
-        nameValuePairs.add(new BasicNameValuePair("expDate", orderItems.get(0)));
-        String items = "" ;
-        for (int i=6; i < orderItems.size(); i++) {
-                items += orderItems.get(i) + "&%&" ;
-        }
-        nameValuePairs.add(new BasicNameValuePair("order", items));
+		try{
+			String urlParameter = "menu=" + menuName;
+			URL url = new URL("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/getMenu.php");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setInstanceFollowRedirects(false);
+			connection.setRequestMethod("POST"); 
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
+			connection.setRequestProperty("charset", "utf-8");
+			connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameter.getBytes().length));
+			connection.setUseCaches (false);
+			
+			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+			wr.writeBytes(urlParameter);
+			wr.flush();
+			wr.close();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			String tmp;
+			while ((tmp = in.readLine()) != null){
+				result = tmp.substring(tmp.indexOf("items")+8, tmp.length()-6);
+			}
+			in.close();
+			
+			connection.disconnect();
+			
+			String[] list = result.split("&&&");
+			loadMenu.add("");
+			for(String item: list){
+				loadMenu.add(item);
+			}
 
+		}catch(Exception e){
+			System.err.println("Error in getting menu names "+e.toString());
+		
+		}
+		return loadMenu ;
+	}
 
-        try{
-                // http post
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost("http://www.cs.colostate.edu/~loidolt/ExWorkFiles/saveOrder.php");
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                HttpResponse response = httpclient.execute(httppost);
-                HttpEntity entity = response.getEntity();
-                InputStream is = entity.getContent();
+	/*
+	 * saves the order to the orders database
+	 * all items are in one field in the database called 'items' delimited by '&%&'
+	 */
+	public static boolean saveOrder(ArrayList<String> orderItems) {
+		boolean success = true ;
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pizza?" +
+					"user=pizzaStore&password=password");
+			Statement stmt = conn.createStatement();
+			String query = "INSERT INTO orders "
+					+ "(name, phone, address, nameOnCard, creditCardNumber, expirationDate, items) VALUES('" ;
+			query += orderItems.get(0) + "', '" ; // name
+			query += orderItems.get(1) + "', '" ; // phone number
+			query += orderItems.get(2) + "', '" ; // address
+			query += orderItems.get(3) + "', '" ; // nameOnCard
+			query += orderItems.get(4) + "', '" ; // creditCardNum
+			query += orderItems.get(5) + "', '" ; // expirationDate
 
-        }catch(Exception e){
-                System.err.println("Error in saving order "+e.toString());
-        }
-        if (success) {
-                System.out.println("Order saved") ;
-        }
-        return success ;
-    }
+			String items = "" ;
+			for (int i=6; i < orderItems.size(); i++) {
+				items += orderItems.get(i) + "&%&" ;
+			}
+			query += items + "') ;" ;
+			stmt.executeUpdate(query) ;
+			stmt.close() ;
+			conn.close() ;
+
+		}
+		catch(Exception e) {
+			System.err.print(e);
+			success = false ;
+		}
+		if (success) {
+			System.out.println("Order saved") ;
+		}
+		return success ;
+	}
 }
